0b70bdc9d9261287c9cd784694457a25f9a54e4c
Judy Long <long.judy92@gmail.com>
2016-07-05 08:22:30 -0400

    setup backbone

---
+++ app/assets/javascripts/hopscotch.js
@@ -0,0 +1,13 @@
+window.Hopscotch = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Hopscotch.initialize();
+});
