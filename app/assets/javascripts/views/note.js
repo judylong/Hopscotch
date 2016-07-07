52ae7edbbc45709a13564460bf41ed313251c2d0
Judy Long <long.judy92@gmail.com>
2016-07-07 13:34:33 -0400

    make notes editable and update note on blur

--- app/assets/javascripts/views/note.js
+++ app/assets/javascripts/views/note.js
@@ -1,9 +1,37 @@
 Hopscotch.Views.Note = Backbone.View.extend({
+  initialize: function() {
+    this.editing = false;
+  },
   template: JST['notes/indexItem'],
   className: "note",
+  events: {
+    "click": "editable",
+    "blur textarea": "updateNote"
+  },
   render: function() {
     var content = this.template({model: this.model});
     this.$el.html(content);
     return this;
+  },
+  editable: function(e) {
+    e.preventDefault();
+    // debugger
+    if (this.editing === false) {
+      this.editing = true;
+      var content = JST['notes/edit']({model: this.model});
+      this.$el.html(content);
+      $('textarea').focus();
+    }
+    return this;
+  },
+  updateNote: function(e) {
+    // debugger
+    attr = e.currentTarget.value;
+    this.model.save("words", attr, {
+      success: function() {
+        this.editing = false;
+        this.render();
+      }.bind(this)
+    });
   }
 });
