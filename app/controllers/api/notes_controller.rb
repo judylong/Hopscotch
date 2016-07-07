482416cb21a0aca4f180bd7bac660090a9560625
Judy Long <long.judy92@gmail.com>
2016-07-07 11:03:57 -0400

    add api namespaced routes and notes controller

---
+++ app/controllers/api/notes_controller.rb
@@ -0,0 +1,5 @@
+module Api
+  class NotesController < ApiController
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: "site#root"
+
+  namespace :api, defaults: {format: :json} do
+    resources :notes
+  end
 end
