0fc27e55ce9a8fde6ccd9580203b3a3a50af25b7
Judy Long <long.judy92@gmail.com>
2016-07-07 11:46:36 -0400

    create backbone note model and collection

---
+++ app/assets/javascripts/collections/notes.js
@@ -0,0 +1,4 @@
+Hopscotch.Collections.Notes = Backbone.Collection.extend({
+  url: 'api/notes',
+  model: Hopscotch.Models.Note
+});
---
+++ app/assets/javascripts/models/note.js
@@ -0,0 +1,3 @@
+Hopscotch.Models.Note = Backbone.Model.extend({
+  urlRoot: 'api/notes'
+});
